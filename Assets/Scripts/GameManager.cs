95f26fa04b886fc2adcf12d7b164261ae9ac07a2
alteus <alteus@hotmail.com>
2016-12-29 18:55:46 +0900

    16.12.29.2
    16.12.29.2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    public float levelStartDelay = 2f;
+    public float turnDelay = 0.1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
 
-
-    private int level = 3;
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 1;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
     void Awake ()
     {
@@ -22,17 +30,56 @@ public class GameManager : MonoBehaviour {
 
         DontDestroyOnLoad(gameObject);
 
+        enemies = new List<Enemy>();
+
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
 
     void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
     }
 
 	// Update is called once per frame
 	void Update () {
-		
+        if (playersTurn || enemiesMoving)
+        {
+            return;
+        }
+
+        StartCoroutine(MoveEnemies());
 	}
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+
+    }
+
+
+
+    IEnumerator MoveEnemies ()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+
+    }
 }
